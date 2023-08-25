b418cfa9a3782362d923c6ecd9de67dba7ba83e8
Sergey Konoplev <sergey.gsarh@gmail.com>
2023-08-26 00:17:47 +0300

    added tests from reqres.in

---
+++ src/test/java/ReqresInHWTests.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInHWTests {
+
+    @Test
+    @DisplayName("Checking the number of all users")
+    void getUsers() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(12));
+    }
+
+    @Test
+    @DisplayName("Checking the name and job when creating a new user")
+    void createUser() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    @DisplayName("Editing the user's place of work")
+    void editUser() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    @DisplayName("Deleting a user")
+    void deleteUser() {
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("Login-unsuccessful")
+    void loginUnsuccessful() {
+        String data = "{ \"email\": \"peter@klaven\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
